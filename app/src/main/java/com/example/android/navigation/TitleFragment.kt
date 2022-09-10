23c59f5950f6466401985b69df8e2af9c3be5310
Muhammad Alaraby <mohamedelaraby1002@gmail.com>
2022-09-10 14:01:24 +0200

    App Navigation-Udacity nanodegree-lesson 3 app.

---
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -0,0 +1,35 @@
+package com.example.android.navigation
+import android.content.Intent
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.example.android.navigation.databinding.FragmentTitleBinding
+class TitleFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?): View? {
+        val binding:FragmentTitleBinding=DataBindingUtil.inflate(inflater,R.layout.fragment_title,container,false)
+        binding.playButton.setOnClickListener{view:View->
+          view.findNavController().navigate(TitleFragmentDirections.actionTitleFragmentToGameFragment())
+        }
+
+        setHasOptionsMenu(true)
+
+
+        return binding.root
+    }//on create view()
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.over_folw_menu,menu)
+    }
+    //we need to override onOptionsItemSelected to connect it to our NavigationUI.
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item,requireView().findNavController())
+                ||super.onOptionsItemSelected(item)
+    }
+
+}
